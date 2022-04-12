7004ef8d5f704df37d76682f4aee23ea733bbb3a
edoaurahman <edoaurahman@gmail.com>
2022-04-12 15:39:00 +0700

    fix font error

--- index.php
+++ index.php
@@ -11,9 +11,6 @@ if (isset($_SESSION['nik'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Log in</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="./assets/css/adminlte.min.css">
 </head>
 
@@ -50,9 +47,9 @@ if (isset($_SESSION['nik'])) {
   <!-- /.login-box -->
 
   <!-- jQuery -->
-  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <script src="./plugins/jquery/jquery.min.js"></script>
   <!-- Bootstrap 4 -->
-  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- AdminLTE App -->
   <script src="./assets/js/adminlte.min.js"></script>
 </body>
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -19,8 +19,6 @@ $banyakTempat = mysqli_num_rows($query);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Dashboard</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
